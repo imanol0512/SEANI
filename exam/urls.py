d5455010aab3be28aa12cb28797f2782150e670d
Imanol <taneima05@gmail.com>
2024-03-13 14:45:31 -0600

    07 Implements navigation in question exam

--- exam/urls.py
+++ exam/urls.py
@@ -4,6 +4,8 @@ from.import views
 app_name="exam"
 urlpatterns = [
     path('create/', views.create, name='create'),
+    path('module/<int:m_id>/question/',views.question,name='question'),
+    path('module/<int:m_id>/question/<int:q_id>/',views.question,name='question'),
     path("",views.home,name="home")
     
 ]
